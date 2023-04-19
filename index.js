36f555e6bd83819541b0a34db26162cb5cc5e115
Kevin Dean <kdean1011@gmail.com>
2023-04-18 23:21:13 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+const names = [];
+
+function writeCards(names){
+    let cards=[]
+    for (let i = 0; i < names.length; i++) {
+        cards.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`)
+    }
+    return cards
+}
+writeCards(names);
+
+function countDown(n){
+    for(let i = 0; i <= n; i++)
+    console.log (i);
+}
+countDown(11)
+countDown(4)
